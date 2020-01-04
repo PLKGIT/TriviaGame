09e6b402478f45c4c89273de910015f06d9b9038
Lammie <plkelly@yahoo.com>
2020-01-04 03:06:22 -0800

    Initial commit after setup.  Created folder structure and index.html, reset.css, style.css, and app.js files. Downloaded background image and added it to the images folder.  Wireframed the game in index.html and applied some basic styling in style.css.  Pseudocoded the game in app.js.

---
+++ assets/javascript/app.js
@@ -0,0 +1,41 @@
+/*
+    Pam Kelly's JavaScript and jQuery Code for Totally Trivial Trivia Game
+*/
+
+// Global Variables
+    //Variable for total number of questions
+    //Counter for correct answers
+    //Counter for incorrect answers
+
+// On Page Load
+    //Show game title
+    //Show START button
+    //Form and Done button hidden
+
+// Listen for Start and Done button clicks
+    //Start button...Call Game Start
+    //Done button...Call Game End
+
+// Game Start
+    //Start the timer
+    //Hide START button
+    //Show questions form
+    //Show DONE button 
+
+// Play Game
+    //Timer counts down
+    //Answer Questions
+        //Only one answer per question (radio buttons)
+    //Game Ends
+        //When timer reaches 0 OR DONE button clicked
+    //Stop Default action for form??
+
+// Game End
+    //Hide questions form
+    //Hide DONE button
+    //Tally correct and incorrect answers
+    //Display Game over message
+    //Display answer statistics
+        //Correct Answers: #
+        //Incorrect Answers: #
+        //Unanswered Questions: #
